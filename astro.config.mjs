aa65bd99b80972a6835d5719f18c42969f88d4ae
mjohansen8 <mj@mercura.dk>
2023-12-20 19:17:34 +0100

    Snitcher Script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
-// import vercel from "@astrojs/vercel/serverless";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "hybrid",
-  integrations: [tailwind()]
+  integrations: [tailwind(), partytown()]
 });
